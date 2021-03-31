3fe49475694780708b91ebaae46bc7b5b840aacf
Nur Yalcin <nur.yalcin.a@gmail.com>
2021-03-31 17:56:25 +0300

    31.03

---
+++ src/app/models/customer.ts
@@ -0,0 +1,5 @@
+export interface Customer{
+    customerId:string;
+    userId:string;
+    companyName: string;
+}
---
+++ src/app/models/rentalDetail.ts
@@ -0,0 +1,9 @@
+export interface RentalDetail{
+    carId:number;
+    carName:string;
+    firstName:string;
+    lastName:string;
+    companyName:string;
+    rentDate:Date;
+    returnDate:Date;
+}
